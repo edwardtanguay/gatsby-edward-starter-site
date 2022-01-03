8eb26066fe06e3282dccef6ba107b638fe016fec
Edward Tanguay (hm.win) <edward@tanguay.info>
2022-01-04 00:31:03 +0100

    finish content

--- src/components/layout.js
+++ src/components/layout.js
@@ -1,19 +1,12 @@
-/**
- * Layout component that queries for data
- * with Gatsby's useStaticQuery component
- *
- * See: https://www.gatsbyjs.com/docs/use-static-query/
- */
-
 import * as React from "react"
 import PropTypes from "prop-types"
 import { useStaticQuery, graphql } from "gatsby"
 
-import Header from "./header"
 import "./layout.css"
+import { Link } from "gatsby"
 
 const Layout = ({ children }) => {
-  const data = useStaticQuery(graphql`
+	const data = useStaticQuery(graphql`
     query SiteTitleQuery {
      site {
       siteMetadata {
@@ -24,33 +17,26 @@ const Layout = ({ children }) => {
     }
   `)
 
-  return (
-    <>
-		  <Header siteTitle={data.site.siteMetadata?.title || `Title`} subtitle={data.site.siteMetadata.subtitle} />
-      <div
-        style={{
-          margin: `0 auto`,
-          maxWidth: 960,
-          padding: `0 1.0875rem 1.45rem`,
-        }}
-      >
-			  <main>{children}</main>
-        <footer
-          style={{
-            marginTop: `2rem`,
-          }}
-        >
-          © {new Date().getFullYear()}, Built with
-          {` `}
-          <a href="https://www.gatsbyjs.com">Gatsby</a>
-        </footer>
-      </div>
-    </>
-  )
+	return (
+		<>
+			<h1>{data.site.siteMetadata.title}</h1>	
+			<Link to="/">Welcome</Link> <br />
+			<Link to="/about">About</Link> <br />
+			<div
+				style={{
+					margin: `0 auto`,
+					maxWidth: 960,
+					padding: `0 1.0875rem 1.45rem`,
+				}}
+			>
+				<main>{children}</main>
+			</div>
+		</>
+	)
 }
 
 Layout.propTypes = {
-  children: PropTypes.node.isRequired,
+	children: PropTypes.node.isRequired,
 }
 
 export default Layout
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,30 +1,12 @@
 import * as React from "react"
-import { Link } from "gatsby"
-import { StaticImage } from "gatsby-plugin-image"
 
 import Layout from "../components/layout"
 import Seo from "../components/seo"
 
 const IndexPage = () => (
 	<Layout>
-		<Seo title="Home" />
-		<h1>Hi everybody</h1>
-		<p>Welcome to your new Gatsby site.</p>
-		<p>Now go build something great.</p>
-		<StaticImage
-			src="../images/gatsby-astronaut.png"
-			width={500}
-			quality={95}
-			formats={["auto", "webp", "avif"]}
-			alt="A Gatsby astronaut"
-			style={{ marginBottom: `1.45rem` }}
-		/>
-		<p>
-			<Link to="/page-2/">Go to page 2</Link> <br />
-			<Link to="/using-typescript/">Go to "Using TypeScript"</Link> <br />
-			<Link to="/using-ssr">Go to "Using SSR"</Link> <br />
-			<Link to="/using-dsg">Go to "Using DSG"</Link>
-		</p>
+		<Seo title="Welcome" />
+		<p>Welcome to our project.</p>
 	</Layout>
 )
 
